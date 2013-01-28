bd1a3e70856b1cb8f5a83f197434091515f68474
Alex Gisby <alex.gisby@gmail.com>
2013-01-28 23:01:13 +0000

    Generating the necessary directories for output to start

--- packerizer.py
+++ packerizer.py
@@ -28,6 +28,20 @@ class Packer(object):
 			# Don't worry, guess at 0.0
 			self.config['base_version'] = u'0.0'
 
+		if 'output_dir' not in self.config:
+			self.config['output_dir'] = os.path.join(self.base_dir, 'output')
+
+		# Check that output exists, if not create.
+		if not os.path.exists(self.config['output_dir']):
+			os.makedirs(self.config['output_dir'])
+
+		# Read the buildinfo file:
+		if os.path.exists(self.buildfile_location):
+			with open(self.config_location) as build_file:
+				self.buildfile = json.load(build_file)
+		else:
+			self.buildfile = dict()
+
 
 	def build_info(self):
 		"""Returns some information about the build we're about to make. TBD."""
@@ -36,5 +50,20 @@ class Packer(object):
 		
 	def compress(self):
 		"""Does the actual file sticking together and compression"""
-		pass
+		
+		# Calculate the minor and full version filenames:
+		minor_version = "0" if 'minor_version' not in self.buildfile else self.buildfile['minor_version']
+		full_version = self.config['base_version'] + '.' + minor_version
+
+		# Make the output directory
+		full_output_dir = os.path.join(self.config['output_dir'], full_version)
+		if not os.path.exists(full_output_dir):
+			os.makedirs(full_output_dir)
+
+		# Create the necessary dirs in the output folder:
+		workspace_dir = os.path.join(full_output_dir, "workspace")
+		if not os.path.exists(workspace_dir):
+			os.makedirs(workspace_dir)
+
+		# Housekeeping done, let's start making some sweet concatenated files!
 
